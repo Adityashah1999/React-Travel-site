bdce0d23e67a4cf77f8ed0853ebf226035d43ed4
Aditya Shah <nischalshah1999@gmail.com>
2021-08-12 22:56:56 +0545

    LastCommit

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import { Button } from './Button';
-import '../Components/Her';
+import './HeroSection.css';
 
 function HeroSection() {
   return (
